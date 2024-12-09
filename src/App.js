31170fb2475077935aeb004cf07501527039cf55
Cruz3196 <cruzjos9619@gmail.com>
2024-12-09 00:51:44 -0800

    Aos added from prev project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import { Home, About, Services, Safety, Contact } from "./pages/index";
 import { Navbar } from "./components/index";
+import Aos from "aos";
+import 'aos/dist/aos.css'; // Import AOS styles globally
+import { useEffect } from 'react';
 import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  useEffect(() => {
+    Aos.init({
+      duration:2000,
+      once: true
+    });
+  },[])
+
   return (
     <Router>
       <Navbar/>
